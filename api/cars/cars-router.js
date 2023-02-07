f06461831bd88b2340b7990c7f2ebe1ffdbe9277
Gözde G <gzde.gurler@gmail.com>
2023-02-07 18:36:58 +0300

    update

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,20 @@
 // HOKUS POKUS
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  try {
+    res.status(200).json({ message: "Çalıştı" });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+  });
+});
+
+module.exports = router;
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,15 @@
 // ESNEK
+
+exports.seed = async function (knex) {
+  await knex("cars").truncate();
+  await knex("cars").insert([
+    {
+      vin: "1GNGC26RXXJ407648",
+      make: "Chevrolet",
+      model: "Suburban",
+      mileage: "1999",
+      title: "Az kullanılmış",
+      transmission: "Manuel",
+    },
+  ]);
+};
