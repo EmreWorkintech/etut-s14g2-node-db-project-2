87a701c08880cb6d95a94a88204316891a539217
Miran San <miran.san@ozu.edu.tr>
2023-02-07 19:58:49 +0300

    finalhali

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,11 +1,33 @@
 // HOKUS POKUS
 const express = require("express");
 
+const carmodels = require ("./cars-model");
+
+const md = require ("./cars-middleware");
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/",  async (req, res, next) => {
   try {
-    res.status(200).json({ message: "Çalıştı" });
+    const cars = await carmodels.getAll();
+    res.json(cars);    
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", md.checkCarId,  (req, res, next) => {
+  try {
+    res.json(req.car);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", md.checkCarPayload, md.checkVinNumberUnique, md.checkVinNumberValid, async (req, res, next) => {
+  try {
+    const car = await carmodels.create(req.body);
+    res.status(201).json(car);
   } catch (error) {
     next(error);
   }
